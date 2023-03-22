a0d052fd630312022b7efe0ae6ec7eaaaa6a19f4
Dwight Caddell <dcaddelljr@gmail.com>
2023-03-22 09:48:15 -0500

    StaticExercise2

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -6,14 +6,15 @@ namespace StaticExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Celsius temperature: ");
-            var userInput1 = double.Parse(Console.ReadLine());
-            Console.WriteLine($"The converted Celsius temperature is  {TempConverter.CelsiusToFahrenheit(userInput1)} Fahrenheit");
+            Console.WriteLine("Enter a Fahrenheit temperature: ");
+            double userInput1 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The converted Fahrenheit temperature is {TempConverter.FahrenheitToCelsius(userInput1)} Celsius");
             Console.WriteLine();
 
-            Console.WriteLine("Enter a Fahrenheit temperature: ");
-            var userInput2 = double.Parse(Console.ReadLine());
-            Console.WriteLine($"The converted Fahrenheit temperature is  {TempConverter.FahrenheitToCelsius(userInput2)} Celsius");
+            Console.WriteLine("Enter a Celsius temperature: ");
+            double userInput2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The converted Celsius temperature is {TempConverter.CelsiusToFahrenheit(userInput2)} Fahrenheit");
+           
         }
     }
 }
--- StaticExercise/TempConverter.cs
+++ StaticExercise/TempConverter.cs
@@ -2,16 +2,12 @@
 namespace StaticExercise
 {
 	public static class TempConverter
-	{
-		public static double CelsiusToFahrenheit(double Celsius)
-        {
-            return (Celsius * 1.8) + 32;
-        }
-     
-        public static double FahrenheitToCelsius(double Fahrenheit)
-        {
-            return (Fahrenheit - 32) * .5556;
-        }
+	{ 
+        public static double FahrenheitToCelsius(double fTemp) => (fTemp - 32.0) * .5556;
+       
+
+        public static double CelsiusToFahrenheit(double cTemp) => (cTemp * 1.8) + 32.0;
+        
     }
 }
 
