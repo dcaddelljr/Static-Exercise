b768877186771682232d6ed7bf8f370bba3f504a
Dwight Caddell <dcaddelljr@gmail.com>
2023-03-21 19:31:52 -0500

    StaticExercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace StaticExercise
+﻿using StaticExercise;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a Celsius temperature: ");
+            var userInput1 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The converted Celsius temperature is  {TempConverter.CelsiusToFahrenheit(userInput1)} Fahrenheit");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a Fahrenheit temperature: ");
+            var userInput2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The converted Fahrenheit temperature is  {TempConverter.FahrenheitToCelsius(userInput2)} Celsius");
         }
     }
 }
+
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double CelsiusToFahrenheit(double Celsius)
+        {
+            return (Celsius * 1.8) + 32;
+        }
+     
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            return (Fahrenheit - 32) * .5556;
+        }
+    }
+}
+
